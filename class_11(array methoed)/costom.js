5938e071cffdda9b9b9b47e9deb38a7ecb186944
nurnaby <sohag.biit@gmail.com>
2022-08-02 11:35:01 +0600

    class 11

--- class_11(array methoed)/costom.js
+++ class_11(array methoed)/costom.js
@@ -1,3 +1,15 @@
 var names = ["atik", "raihan", "polash", "tuhin"]
-var pushNames = names.push("nayon");
+var pushNames = names.push("nayon"); //push( ) methods
+// console.log(names);
+
+var pushNames = names.pop(); //pop( ) methods
+//console.log(names); 
+
+var pushNames = names.shift(); //shift( ) methods
+// console.log(names);
+
+var pushNames = names.splice(2, 0, "shoag", "jabad");
+// console.log(names);
+
+var pushNames = names.splice(1, 2);
 console.log(names);
