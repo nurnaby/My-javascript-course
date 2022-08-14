217b394c697f1af675a1ffbb117f222dcdb11a0a
nurnaby <sohag.biit@gmail.com>
2022-08-14 22:21:03 +0600

    class 18

---
+++ class_18(moth,)/costom.js
@@ -0,0 +1,15 @@
+var n = 5.7;
+// Math.round(x) returns the nearest integer: 
+console.log(Math.round(n)) + "</br>"
+    // Math.ceil(x) returns the value of x rounded up to its nearest integer:  
+console.log(Math.ceil(n)) + "</br>"
+    // Math.floor(x) returns the value of x rounded down to its nearest integer: 
+console.log(Math.floor(n)) + "</br>"
+    // Math.sign(x) returns if x is negative, null or positive: 
+console.log(Math.sign(n))
+    // Returns a random integer from 1 to 6:
+var ran = Math.floor(Math.random() * 6 + 1)
+
+console.log(ran)
+
+// console.log(Math.sign(n))
